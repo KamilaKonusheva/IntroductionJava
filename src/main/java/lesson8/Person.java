03d2ec644417f4023dae2416b844adf6ff52940c
KamilaKonusheva <67046552+KamilaKonusheva@users.noreply.github.com>
2020-07-14 16:32:12 +0300

    Update Person.java

--- src/main/java/lesson8/Person.java
+++ src/main/java/lesson8/Person.java
@@ -2,7 +2,7 @@ package lesson8;
 
 public class Person {
     String name;
-    char gender;
+    int code;
     byte age;
     float height;
     float weight;
@@ -10,6 +10,83 @@ public class Person {
     int phone;
     short flatNumber;
     int insurance;
+
+    public Person() {
+    }
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public Person(String name, int code) {
+        this.name = name;
+        this.code = code;
+    }
+
+    public Person(String name, int code, byte age) {
+        this.name = name;
+        this.code = code;
+        this.age = age;
+    }
+
+    public Person(String name, int code, byte age, float height) {
+        this.name = name;
+        this.code = code;
+        this.age = age;
+        this.height = height;
+    }
+
+    public Person(String name, int code, byte age, float height, float weight) {
+        this.name = name;
+        this.code = code;
+        this.age = age;
+        this.height = height;
+        this.weight = weight;
+    }
+
+    public Person(String name, int code, byte age, float height, float weight, int card) {
+        this.name = name;
+        this.code = code;
+        this.age = age;
+        this.height = height;
+        this.weight = weight;
+        this.card = card;
+    }
+
+    public Person(String name, int code, byte age, float height, float weight, int card, int phone) {
+        this.name = name;
+        this.code = code;
+        this.age = age;
+        this.height = height;
+        this.weight = weight;
+        this.card = card;
+        this.phone = phone;
+    }
+
+    public Person(String name, int code, byte age, float height, float weight, 
+                  int card, int phone, short flatNumber) {
+        this.name = name;
+        this.code = code;
+        this.age = age;
+        this.height = height;
+        this.weight = weight;
+        this.card = card;
+        this.phone = phone;
+        this.flatNumber = flatNumber;
+    }
+
+    public Person(String name, int code, byte age, float height, float weight, 
+                  int card, int phone, short flatNumber, int insurance) {
+        this.name = name;
+        this.code = code;
+        this.age = age;
+        this.height = height;
+        this.weight = weight;
+        this.card = card;
+        this.phone = phone;
+        this.flatNumber = flatNumber;
+        this.insurance = insurance;
+    }
 
     public String getName() {
         return name;
@@ -19,12 +96,12 @@ public class Person {
         this.name = name;
     }
 
-    public char getGender() {
-        return gender;
+    public int getCode() {
+        return code;
     }
 
-    public void setGender(char gender) {
-        this.gender = gender;
+    public void setCode(int code) {
+        this.code = code;
     }
 
     public byte getAge() {
@@ -83,80 +160,18 @@ public class Person {
         this.insurance = insurance;
     }
 
-    public Person() {
-    }
-
-    public Person(String name) {
-        this.name = name;
-    }
-
-    public Person(String name, char gender) {
-        this.name = name;
-        this.gender = gender;
-    }
-
-    public Person(String name, char gender, byte age) {
-        this.name = name;
-        this.gender = gender;
-        this.age = age;
-    }
-
-    public Person(String name, char gender, byte age, float height) {
-        this.name = name;
-        this.gender = gender;
-        this.age = age;
-        this.height = height;
-    }
-
-    public Person(String name, char gender, byte age, float height, float weight) {
-        this.name = name;
-        this.gender = gender;
-        this.age = age;
-        this.height = height;
-        this.weight = weight;
-    }
-
-    public Person(String name, char gender, byte age, float height, float weight, int card) {
-        this.name = name;
-        this.gender = gender;
-        this.age = age;
-        this.height = height;
-        this.weight = weight;
-        this.card = card;
-    }
-
-    public Person(String name, char gender, byte age, float height, float weight, int card, int phone) {
-        this.name = name;
-        this.gender = gender;
-        this.age = age;
-        this.height = height;
-        this.weight = weight;
-        this.card = card;
-        this.phone = phone;
-    }
-
-    public Person(String name, char gender, byte age, float height, float weight,
-                  int card, int phone, short flatNumber) {
-        this.name = name;
-        this.gender = gender;
-        this.age = age;
-        this.height = height;
-        this.weight = weight;
-        this.card = card;
-        this.phone = phone;
-        this.flatNumber = flatNumber;
-    }
-
-    public Person(String name, char gender, byte age, float height, float weight,
-                  int card, int phone, short flatNumber, int insurance) {
-        this.name = name;
-        this.gender = gender;
-        this.age = age;
-        this.height = height;
-        this.weight = weight;
-        this.card = card;
-        this.phone = phone;
-        this.flatNumber = flatNumber;
-        this.insurance = insurance;
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", code=" + code +
+                ", age=" + age +
+                ", height=" + height +
+                ", weight=" + weight +
+                ", card=" + card +
+                ", phone=" + phone +
+                ", flatNumber=" + flatNumber +
+                ", insurance=" + insurance +
+                '}';
     }
 }
