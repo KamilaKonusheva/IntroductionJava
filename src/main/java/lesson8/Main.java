b9b7d31a4d4d6bdf0a92fdb5177ce9e9a3e2ef04
KamilaKonusheva <67046552+KamilaKonusheva@users.noreply.github.com>
2020-07-14 16:33:32 +0300

    Update Main.java

--- src/main/java/lesson8/Main.java
+++ src/main/java/lesson8/Main.java
@@ -3,5 +3,6 @@ package lesson8;
 public class Main {
     public static void main(String[] args) {
         System.out.println(new FinalExample("Alex", "Black", 22));
+        System.out.println(new Person("David",66));
     }
 }
